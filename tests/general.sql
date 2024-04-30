75f993402d3b91deae695ca66dbd6ac1c16293c9
niolap <nicolas@agoston.io>
2024-04-30 14:48:38 +0200

    progress

---
+++ tests/general.sql
@@ -0,0 +1,100 @@
+\set ON_ERROR_STOP on
+
+------------------------------------------------------------------------
+-- Postgres user
+------------------------------------------------------------------------
+set role postgres;
+
+do $$
+declare
+    v_backend_old_version varchar(10);
+    v_backend_new_version varchar(10);
+    v_backend_test_version varchar(10) := 'v0.0.0';
+begin
+
+    select agoston_api.get_backend_version() into v_backend_old_version;
+    select agoston_api.set_backend_version( p_version => v_backend_test_version ) into v_backend_new_version;
+    assert v_backend_new_version = v_backend_test_version;
+    select agoston_api.set_backend_version( p_version => v_backend_old_version ) into v_backend_new_version;
+    assert v_backend_new_version = v_backend_old_version;
+
+end $$;
+
+------------------------------------------------------------------------
+-- Postgraphile user
+------------------------------------------------------------------------
+set role :postgraphile_user;
+
+do $$
+declare
+    v_backend_version varchar(10);
+begin
+
+    perform agoston_api.set_authenticated_user(
+        p_provider => 'user-pwd',
+        p_subject => '848456qsdqs56d56qs',
+        p_raw => '{"username":"niolap","more_data":{"attr1":"val1"}}',
+        p_password => 'aFakePassword'
+    );
+    perform agoston_api.set_authenticated_user(
+        p_provider => 'google',
+        p_subject => 'niolap',
+        p_raw => '{"username":"niolap","more_data":{"attr1":"val1"}}'
+    );
+
+end $$;
+
+------------------------------------------------------------------------
+-- Developer user
+------------------------------------------------------------------------
+set role :developer_user;
+
+do $$
+declare
+    v_user_id integer;
+    v_role_id integer;
+    v_role_anonymous_id integer;
+    v_role_authenticated_id integer;
+    v_user_token text;
+    v_job record;
+    v_table_name text;
+begin
+
+    -- Session
+    perform agoston_public.session();
+
+    -- Users
+    select agoston_api.add_user () into v_user_id;
+    select agoston_api.set_user_token (p_user_id => v_user_id) into v_user_token;
+    perform agoston_api.get_user_by_token (v_user_token);
+    perform agoston_api.delete_user (v_user_id);
+
+end $$;
+
+-- Jobs
+do $$
+declare
+    v_job record;
+begin
+    select agoston_api.add_job( identifier => 'run-sql', payload => '{ "sql": "create table \"added_job_table\" ( id int );" }') into v_job;
+end $$;
+select pg_sleep(2); -- Let's give a few seconds to the worker.
+
+
+do $$
+declare
+    v_table_name text;
+begin
+    select tablename into v_table_name from pg_tables where schemaname = 'agoston_public' and tablename  = 'added_job_table';
+    assert v_table_name = 'added_job_table';
+end $$;
+
+-- Cron Jobs
+do $$
+declare
+    v_crontabs record;
+begin
+    select agoston_api.add_cron_job( task => 'run-sql', identifier => 'run-sql-1', pattern => '* * * * *', payload => '{ "sql": "insert into added_job_table values (1);" }') into v_crontabs;
+    select agoston_api.add_cron_job( task => 'run-sql', identifier => 'run-sql-2', pattern => '* * * * *', payload => '{ "sql": "insert into added_job_table values (1);" }') into v_crontabs;
+    select agoston_api.add_cron_job( task => 'run-sql', identifier => 'run-sql-3', pattern => '* * * * *', payload => '{ "sql": "insert into added_job_table values (1);" }') into v_crontabs;
+end $$;
---
+++ tests/run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Function
+function backend_start () {
+    docker compose -f ./docker-compose.yml up -d --wait
+}
+
+function backend_stop () {
+    docker compose -f ./docker-compose.yml kill
+}
+
+function backend_remove () {
+    docker rm agoston-postgraphile-postgraphile-1 agoston-postgraphile-postgres-1
+    docker rmi -f agoston-postgraphile-dev agoston-postgres-dev
+}
+
+function run_sql_tests () {
+    docker exec -it -u postgres agoston-postgraphile-postgres-1 \
+        psql agoston \
+        -f /tests/${1}.sql \
+        -v developer_user=developer \
+        -v postgraphile_user=postgraphile
+}
+
+function run_auth_tests () {
+    curl -k -X POST -d 'username=anyuser&password=s2ssDD3hf3-' -c /tmp/cookie.txt https://localhost:8043/auth/user-pwd
+    returned_code=$(curl -k -X POST --cookie /tmp/cookie.txt -s -o /dev/null -w "%{http_code}" https://localhost:8043/auth/logout)
+    if [ $returned_code -ne 201 ]; then
+        echo "Error while logging out of the session!"; exit 1
+    fi
+    returned_code=$(curl -k -X POST --cookie /tmp/cookie.txt -s -o /dev/null -w "%{http_code}" https://localhost:8043/auth/logout)
+    if [ $returned_code -ne 404 ]; then
+        echo "Error while deleting a none existing session!"; exit 1
+    fi
+}
+
+function run_configuration_tests () {
+
+    token="$(docker exec -i -u postgres agoston-postgraphile-postgres-1 psql agoston -c "select 'token:'||set_user_token(p_user_id => agoston_api.add_user());"|grep 'token:'|awk -F':' '{print $2}')"
+
+    role_detected=$(curl -k -s -X GET \
+        -H "Content-Type: application/json" \
+        "https://localhost:8043/.well-known/configuration?gq=query%7Bsession%7D" | jq -r '.currentSession.role')
+    if [[ "${role_detected}" != "anonymous" ]]; then
+        echo "role_detected != anonymous"; exit 1
+    fi
+
+    role_detected=$(curl -k -s -X GET \
+        -H "Content-Type: application/json" \
+        "https://localhost:8043/.well-known/configuration?gq=query%7Bsession%7D" | jq -r '.customGraphQLQueryResult.data.session.role')
+    if [[ "${role_detected}" != "anonymous" ]]; then
+        echo "role_detected != anonymous"; exit 1
+    fi
+
+    role_detected=$(curl -k -s -X GET \
+        -H "Authorization: Bearer ${token}" \
+        -H "Content-Type: application/json" \
+        "https://localhost:8043/.well-known/configuration?gq=query%7Bsession%7D" | jq -r '.currentSession.role')
+    if [[ "${role_detected}" != "authenticated" ]]; then
+        echo "role_detected != authenticated"; exit 1
+    fi
+
+    role_detected=$(curl -k -s -X GET \
+        -H "Authorization: Bearer ${token}" \
+        -H "Content-Type: application/json" \
+        "https://localhost:8043/.well-known/configuration?gq=query%7Bsession%7D" | jq -r '.customGraphQLQueryResult.data.session.role')
+    if [[ "${role_detected}" != "authenticated" ]]; then
+        echo "role_detected != authenticated"; exit 1
+    fi
+}
+
+
+# Program
+echo "START"
+backend_stop
+backend_remove
+backend_start
+run_sql_tests "general"
+run_auth_tests
+backend_stop
+backend_start
+run_sql_tests "test-cron-job"
+run_configuration_tests
+if [[ ${SKIP_FINAL_CLEANUP} -ne 1 ]]; then
+    backend_stop
+fi
+echo "END"
